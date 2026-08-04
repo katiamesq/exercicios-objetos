7b1d440e813efaa5f934493ca1fb3027aa61f9ff
Katia Mesquita <katiamesquita@hotmail.com>
2026-08-04 14:06:01 -0300

    ...

---
+++ 03/index.js
@@ -0,0 +1,9 @@
+const usuario = {
+    nome: "João",
+    idade: 25,
+    profissao: "programador",
+    cor_preferida: "verde"
+}
+
+const mensagem = `Olá... Eu sou ${usuario.nome} e tenho ${usuario.idade} anos de idade. Sou ${usuario.profissao} e minha cor favorita é ${usuario.cor_preferida}!`
+console.log (mensagem)
