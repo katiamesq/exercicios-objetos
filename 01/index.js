d6b03ae46d73c0afc591dc390d6e0f0feb0d0662
Katia Mesquita <katiamesquita@hotmail.com>
2026-08-04 13:20:10 -0300

    iniciando objetos

---
+++ 01/index.js
@@ -0,0 +1,26 @@
+const endereco = {
+    rua : "amora",
+    numero : 22,
+    complemento : "Apto. 22",
+    bairro : "Amoreira",
+    cep : 50000000,
+    cidade : "Natal",
+    estado : "RN",
+    pais : "Brasil"  
+    
+}
+       
+    const usuario = {
+    nome : "Ana Maria",
+    email : "anamaria@gmail.com",
+    telefone : 9999.9999,
+    data_nascimento : "02/02/2002",
+    endereco : endereco
+    }
+    
+//usuario.nome = "Mary"
+//console.log ( usuario.nome ) 
+
+console.log ("Usuário :", usuario)
+
+
